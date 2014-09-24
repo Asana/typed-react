099ac7389547fd5ddc17b9c256766225c64a47bb
Josh Smith <josh@asana.com>
2014-09-24 12:12:18 -0700

    Add tslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var path = require('path');
+var tslint = require('gulp-tslint');
 
 var dirs = {
   src: path.join(__dirname, 'src'),
@@ -10,3 +11,9 @@ var files = {
   src: path.join(dirs.src, '**', '*.ts'),
   test: path.join(dirs.test, '**', '*.ts')
 };
+
+gulp.task('lint', function() {
+  return gulp.src([files.src, files.test])
+    .pipe(tslint())
+    .pipe(tslint.report('prose'));
+});
