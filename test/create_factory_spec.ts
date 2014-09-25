95e0517fb7fc5764a057761b8c4e1a7bc25b892d
Phips Peter <pspeter333@gmail.com>
2014-09-24 18:19:44 -0700

    Added a simple test for factory
    This will be nicer when we can test Dom Nodes with TestUtils

--- test/create_factory_spec.ts
+++ test/create_factory_spec.ts
@@ -1,8 +1,40 @@
-/// <reference path="../typings/mocha/mocha.d.ts" />
-import chai = require("chai");
+/// <reference path="../typings/jest/jest.d.ts" />
+jest.autoMockOff();
 import createFactory = require("../src/create_factory");
 import Component = require("../src/component");
+import React = require("react");
+
+interface FactoryProps {
+    name: string;
+}
+
+interface FactoryState {
+}
+
+class FactoryTest extends Component<FactoryProps, FactoryState> {
+    render() {
+        return React.DOM.h1(null, "Greetings", name);
+    }
+}
 
 describe("createFactory", () => {
+    var factory: React.Factory<FactoryProps>;
+    var descriptor: React.Descriptor<FactoryProps>;
+    var name = "jest";
 
+    beforeEach(() => {
+        factory = createFactory(FactoryTest);
+        descriptor = factory({
+            name: name
+        });
+    });
+
+    it("should be a factory", () => {
+        expect(React.isValidClass(factory)).toBeTruthy();
+    });
+
+    it("should produce a valid descriptor", () => {
+        expect(React.isValidComponent(descriptor)).toBeTruthy();
+        expect(descriptor.props.name).toEqual(name);
+    });
 });
