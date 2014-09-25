da8a777ce200407ef0b73941f3c13852a3ac6911
Phips Peter <pspeter333@gmail.com>
2014-09-24 17:29:35 -0700

    Started createFactory
    Need to write tests now

---
+++ test/create_factory_spec.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+import chai = require("chai");
+import createFactory = require("../src/create_factory");
+import Component = require("../src/component");
+
+describe("createFactory", () => {
+
+});
